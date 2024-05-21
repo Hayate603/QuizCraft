7bb50c355e31a43f6d5e8caf6c8398e29fcc0e64
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-05-21 15:19:35 +0900

    [update]ログインページの日本語化に伴い、ログインのテストも日本語に修正

--- spec/system/users/sign_in_spec.rb
+++ spec/system/users/sign_in_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe "ログイン", type: :system do
   context "有効な情報が入力された場合" do
     it "ユーザーがログインできること" do
       visit new_user_session_path
-      fill_in "Email", with: user_email
-      fill_in "Password", with: user_password
-      click_button "Log in"
+      fill_in "メールアドレス", with: user_email
+      fill_in "パスワード", with: user_password
+      click_button "ログイン"
       expect(page).to have_content("ログインしました。")
     end
   end
@@ -18,33 +18,33 @@ RSpec.describe "ログイン", type: :system do
   context "無効な情報が入力された場合" do
     it "ユーザーが無効なパスワードでログインできないこと" do
       visit new_user_session_path
-      fill_in "Email", with: user_email
-      fill_in "Password", with: "wrongpassword"
-      click_button "Log in"
+      fill_in "メールアドレス", with: user_email
+      fill_in "パスワード", with: "wrongpassword"
+      click_button "ログイン"
       expect(page).to have_content("メールアドレスまたはパスワードが違います。")
     end
 
     it "ユーザーが存在しないメールアドレスでログインできないこと" do
       visit new_user_session_path
-      fill_in "Email", with: "nonexistent@example.com"
-      fill_in "Password", with: "password123"
-      click_button "Log in"
+      fill_in "メールアドレス", with: "nonexistent@example.com"
+      fill_in "パスワード", with: "password123"
+      click_button "ログイン"
       expect(page).to have_content("メールアドレスまたはパスワードが違います。")
     end
 
     it "メールアドレスが空の場合にログインできないこと" do
       visit new_user_session_path
-      fill_in "Email", with: ""
-      fill_in "Password", with: user_password
-      click_button "Log in"
+      fill_in "メールアドレス", with: ""
+      fill_in "パスワード", with: user_password
+      click_button "ログイン"
       expect(page).to have_content("メールアドレスまたはパスワードが違います。")
     end
 
     it "パスワードが空の場合にログインできないこと" do
       visit new_user_session_path
-      fill_in "Email", with: user_email
-      fill_in "Password", with: ""
-      click_button "Log in"
+      fill_in "メールアドレス", with: user_email
+      fill_in "パスワード", with: ""
+      click_button "ログイン"
       expect(page).to have_content("メールアドレスまたはパスワードが違います。")
     end
   end
