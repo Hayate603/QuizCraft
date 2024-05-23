a2b97e1e7c5b8665eab40ca852f1485a2d695d71
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-05-23 20:49:23 +0900

    [update]カスタムUsers::RegistrationsControllerを使用するようにルーティングを更新
    - Deviseがカスタムコントローラを使用するように設定
    - devise_scopeの下でedit, update, destroyアクションのカスタムルートを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'users/:id/edit', to: 'users/registrations#edit', as: 'edit_user'
+    patch 'users', to: 'users/registrations#update', as: 'update_user_registration'
+    delete 'users/:id', to: 'users/registrations#destroy', as: 'user'
+  end
   root 'home#index'
 end
