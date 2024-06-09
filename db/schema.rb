37c6ed1e3f3bc7568602f4897b1fd1557f01d53c
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-09 18:34:25 +0900

    [add]ユーザーテーブルにOmniauth関連カラムを追加するマイグレーションとスキーマを更新
    - provider, uid, usernameカラムを追加
    - uidとproviderの組み合わせにユニークインデックスを追加
    - スキーマファイルを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_21_114506) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_114046) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,9 +26,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_21_114506) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.string "provider"
+    t.string "uid"
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
