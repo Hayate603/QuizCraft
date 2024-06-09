aad1e7a41ea699f9b311a677f693394d77141bb8
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-09 20:56:14 +0900

    [add]UserモデルにSNS認証関連のメソッドを追加
    - User.from_omniauthメソッドを追加し、Omniauthの認証データを使用してユーザーを検索または作成
    - Google、Facebookのプロバイダをサポートするように更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable
+         :confirmable, :lockable, :omniauthable,
+         omniauth_providers: [:google_oauth2, :facebook]
+
+  def self.from_omniauth(auth)
+    user = User.find_by(email: auth.info.email)
+  
+    if user
+      user.update(provider: auth.provider, uid: auth.uid, username: auth.info.name)
+    else
+      user = User.create!(
+        provider: auth.provider,
+        uid: auth.uid,
+        username: auth.info.name,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+      user.skip_confirmation! if user.respond_to?(:skip_confirmation!)
+      user.save!
+    end
+  
+    user
+  rescue Net::OpenTimeout
+    nil
+  end
 end
