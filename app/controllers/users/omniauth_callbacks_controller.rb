f28b08c59d9c7a14d91874908fd4cc3599a1f2c5
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-12 20:49:08 +0900

    [update]OmniauthCallbacksControllerのリファクタリングとロケールテキストの移動
    - `handle_auth` メソッドを分割して、サイズを縮小
    - ロケールテキストを `config/locales` ファイルに移動
    - タイムアウト処理を `handle_timeout` メソッドに移動
    これにより、Rubocopの警告（Metrics/AbcSize、Metrics/MethodLength、Rails/I18nLocaleTexts）を解消しました。

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,19 +8,33 @@ module Users
       handle_auth "Facebook"
     end
 
+    private
+
     def handle_auth(kind)
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
-        flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind:)
-        sign_in_and_redirect @user, event: :authentication
+        process_successful_auth(kind)
       else
-        Rails.logger.debug { "User could not be saved: #{@user.errors.full_messages}" }
-        Rails.logger.debug { "OmniAuth data: #{request.env['omniauth.auth']}" }
-        session['devise.auth_data'] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
+        process_failed_auth
       end
     rescue Net::OpenTimeout
-      flash[:alert] = "認証中にタイムアウトが発生しました。もう一度お試しください。"
+      handle_timeout
+    end
+
+    def process_successful_auth(kind)
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: kind)
+      sign_in_and_redirect @user, event: :authentication
+    end
+
+    def process_failed_auth
+      Rails.logger.debug { "User could not be saved: #{@user.errors.full_messages}" }
+      Rails.logger.debug { "OmniAuth data: #{request.env['omniauth.auth']}" }
+      session['devise.auth_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+
+    def handle_timeout
+      flash[:alert] = I18n.t('devise.omniauth_callbacks.timeout')
       redirect_to new_user_registration_url
     end
   end
