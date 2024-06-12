f7de6cf6625015478d49f5a9b00cc27ac4fd2a0e
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-12 17:36:18 +0900

    [update]OmniauthCallbacksControllerのリファクタリング
    - クラス定義をモジュールネスト形式に変更
    - ログメッセージにブロック構文を使用

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,21 +1,21 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    handle_auth "Google"
-  end
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def google_oauth2
+      handle_auth "Google"
+    end
 
-  def facebook
-    handle_auth "Facebook"
-  end
+    def facebook
+      handle_auth "Facebook"
+    end
 
-  def handle_auth(kind)
-    begin
+    def handle_auth(kind)
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
+        flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind:)
         sign_in_and_redirect @user, event: :authentication
       else
-        Rails.logger.debug "User could not be saved: #{@user.errors.full_messages}"
-        Rails.logger.debug "OmniAuth data: #{request.env['omniauth.auth']}"
+        Rails.logger.debug { "User could not be saved: #{@user.errors.full_messages}" }
+        Rails.logger.debug { "OmniAuth data: #{request.env['omniauth.auth']}" }
         session['devise.auth_data'] = request.env['omniauth.auth']
         redirect_to new_user_registration_url
       end
