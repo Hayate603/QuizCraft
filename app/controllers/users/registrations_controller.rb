351fcfe7c03fc716278b6b403282d827510c60dd
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-12 17:39:08 +0900

    [update]RegistrationsControllerのリファクタリング
    - クラス定義をモジュールネスト形式に変更
    - ガード節を使用して認証ユーザーのチェックを修正
    - ガード節にリダイレクトを含める

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,13 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
-  before_action :authorize_user, only: [:edit]
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :authenticate_user!, only: %i[edit update destroy]
+    before_action :authorize_user, only: [:edit]
 
-  private
+    private
 
-  def authorize_user
-    user = User.find_by(id: params[:id])
-    if (user == nil) || (current_user != user)
-      redirect_to root_path, alert: "アクセス権がありません。"
+    def authorize_user
+      user = User.find_by(id: params[:id])
+      return redirect_to(root_path, alert: "アクセス権がありません。") if user.nil? || (current_user != user)
     end
   end
 end
