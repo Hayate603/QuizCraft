68811963b65d0e2404cc30e3eaf4081b672d4a47
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-19 17:44:49 +0900

    [add]クイズコントローラーのアクションを定義
    - index, show, new, createアクションを定義
    - クイズパラメータの許可を設定
    - before_action :authenticate_user! を new, create アクションに追加

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,13 +1,30 @@
 class QuizzesController < ApplicationController
-  def new
-  end
-
-  def create
-  end
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @quizzes = Quiz.all
   end
 
   def show
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def new
+    @quiz = Quiz.new
+  end
+
+  def create
+    @quiz = current_user.quizzes.build(quiz_params)
+    if @quiz.save
+      redirect_to @quiz, notice: 'クイズが作成されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:title, :description)
   end
 end
