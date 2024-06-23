47164d77c17f4bad1dffb74fbef98b165aa86832
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-23 14:27:54 +0900

    [add]Quizモデルのバリデーションテストを追加
    - 他のユーザーなら同じタイトルのクイズを作成できることのテスト
    - 同じユーザーが同じタイトルのクイズを作成できないことのテスト

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -2,23 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
   let(:user) { FactoryBot.create(:user) }
+  let(:another_user) { FactoryBot.create(:user) }
 
   describe 'バリデーションに関するテスト' do
-    it 'タイトルと説明があれば有効であること' do
-      quiz = Quiz.new(title: '有効なタイトル', description: '有効な説明', user: user)
-      expect(quiz).to be_valid
+    context '有効な場合' do
+      it 'タイトルと説明があれば有効であること' do
+        quiz = Quiz.new(title: '有効なタイトル', description: '有効な説明', user: user)
+        expect(quiz).to be_valid
+      end
+
+      it '他のユーザーなら同じタイトルのクイズでも作成できること' do
+        Quiz.create!(title: '同じタイトル', description: '有効な説明', user: user)
+        quiz = Quiz.new(title: '同じタイトル', description: '有効な説明', user: another_user)
+        expect(quiz).to be_valid
+      end
     end
 
-    it 'タイトルがなければ無効であること' do
-      quiz = Quiz.new(description: '有効な説明', user: user)
-      quiz.valid?
-      expect(quiz.errors[:title]).to include("タイトルを入力してください")
-    end
+    context '無効な場合' do
+      it 'タイトルがなければ無効であること' do
+        quiz = Quiz.new(description: '有効な説明', user: user)
+        quiz.valid?
+        expect(quiz.errors[:title]).to include("タイトルを入力してください")
+      end
 
-    it '説明がなければ無効であること' do
-      quiz = Quiz.new(title: '有効なタイトル', user: user)
-      quiz.valid?
-      expect(quiz.errors[:description]).to include("説明を入力してください")
+      it '説明がなければ無効であること' do
+        quiz = Quiz.new(title: '有効なタイトル', user: user)
+        quiz.valid?
+        expect(quiz.errors[:description]).to include("説明を入力してください")
+      end
+
+      it '同じユーザーが同じタイトルのクイズを作成できないこと' do
+        Quiz.create!(title: '同じタイトル', description: '有効な説明', user: user)
+        quiz = Quiz.new(title: '同じタイトル', description: '有効な説明', user: user)
+        quiz.valid?
+        expect(quiz.errors[:title]).to include("同じタイトルというタイトルのクイズは既に存在します。別のタイトルを入力してください。")
+      end
     end
   end
 
