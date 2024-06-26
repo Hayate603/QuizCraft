ee44d68ffe204e0a533638cf5f872cd627e554a3
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-26 18:09:38 +0900

    [add]質問コントローラーにアクションとset_question メソッドを追加
    - edit, update, destroy アクションを追加
    - set_question メソッドを追加し、該当アクションに before_action を設定
    - set_question メソッドで質問を取得している

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit update destroy]
+  before_action :set_question, only: %i[show edit update destroy]
 
   def index
     @questions = Question.all
@@ -19,10 +20,29 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question, notice: I18n.t('notices.question_updated')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_url, notice: I18n.t('notices.question_deleted')
   end
 
   private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
 
   def question_params
     params.require(:question).permit(:question_text, :correct_answer)
