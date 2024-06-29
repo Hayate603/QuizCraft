d1741481ffb8acaee1f050bd3580df5cc78d12b1
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-06-29 17:11:54 +0900

    [update]QuestionsControllerにユーザー認証とリダイレクト先の修正を追加
    - `authorize_user!`メソッドを追加し、質問の編集、更新、および削除時にユーザーが認可されていることを確認。
    - 質問作成時に、`Question.new`を使用し、質問をクイズに追加するように修正。
    - 質問の表示、編集、更新時に関連するクイズを設定するコードを追加。
    - 質問の更新、削除後のリダイレクト先をクイズ詳細ページに変更。

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,37 +2,42 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit update destroy]
   before_action :set_question, only: %i[show edit update destroy]
   before_action :set_quiz, only: %i[new create]
+  before_action :authorize_user!, only: %i[edit update destroy]
 
   def new
     @question = @quiz.questions.build
   end
 
   def create
-    @question = @quiz.questions.build(question_params)
+    @question = Question.new(question_params)
+    @question.quizzes << @quiz
     if @question.save
-      redirect_to [@quiz, @question], notice: I18n.t('notices.question_created')
+      redirect_to quiz_path(@quiz), notice: I18n.t('notices.question_created')
     else
       render :new
     end
   end
 
   def show
+    @quiz = @question.quizzes.first
   end
 
   def edit
+    @quiz = @question.quizzes.first
   end
 
   def update
     if @question.update(question_params)
-      redirect_to @question, notice: I18n.t('notices.question_updated')
+      redirect_to quiz_question_path(@question.quizzes.first, @question), notice: I18n.t('notices.question_updated')
     else
       render :edit
     end
   end
 
   def destroy
+    quiz = @question.quizzes.first
     @question.destroy
-    redirect_to questions_url, notice: I18n.t('notices.question_deleted')
+    redirect_to quiz_path(quiz), notice: I18n.t('notices.question_deleted')
   end
 
   private
@@ -47,5 +52,11 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:question_text, :correct_answer)
+  end
+
+  def authorize_user!
+    unless @question.quizzes.first.user == current_user
+      redirect_to root_path, alert: I18n.t('alerts.access_denied')
+    end
   end
 end
