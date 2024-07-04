80e74af7a3b421b8a8fbe7a05a8a6789b3fe483b
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-04 10:44:05 +0900

    [update]ルーティングを追加
    - take、results、start、resume アクションのルートを追加
    - クイズセッションと回答のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
   devise_scope :user do
     get 'users/:id/edit', to: 'users/registrations#edit', as: 'edit_user'
     patch 'users', to: 'users/registrations#update', as: 'update_user_registration'
@@ -12,6 +13,15 @@ Rails.application.routes.draw do
   root 'quizzes#index'
 
   resources :quizzes do
-    resources :questions, only: %i[new create show edit update destroy]
+    member do
+      get 'take', to: 'quizzes#take'
+      get 'results', to: 'quizzes#results', as: 'results'
+      get 'start', to: 'quizzes#start', as: 'start'
+      get 'resume', to: 'quizzes#resume', as: 'resume'
+    end
+    resources :questions, only: %i[new create show edit update destroy] do
+      resources :user_answers, only: [:create]
+    end
+    resources :quiz_sessions, only: [:create, :update]
   end
 end
