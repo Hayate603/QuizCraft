124e2a1e343de249ee513ac939cbf447b19b939b
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-04 19:21:58 +0900

    [update]quizzes_controllerのリファクタリング
    - 既存セッション検索のロジックをfind_existing_sessionに分割
    - セッション再開判定ロジックをresume_session?に分割
    - 新セッション開始ロジックをstart_new_sessionに分割
    - 最新の完了セッション検索ロジックをfind_last_completed_sessionに分割
    - 既存セッション終了ロジックをend_existing_sessionに分割
    - 次の質問検索ロジックをfind_next_questionに分割

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -38,19 +38,17 @@ class QuizzesController < ApplicationController
   end
 
   def take
-    @existing_session = QuizSession.find_by(user: current_user, quiz: @quiz, end_time: nil)
-    if @existing_session && params[:resume_session] != "false"
+    @existing_session = find_existing_session
+    if resume_session?
       @resume_session = true
     else
-      @existing_session ||= QuizSession.create(user: current_user, quiz: @quiz, start_time: Time.current)
-      @question = params[:question_id] ? @quiz.questions.find(params[:question_id]) : @quiz.questions.first
-      @user_answer = UserAnswer.new
+      start_new_session
     end
   end
 
   def results
-    @quiz_session = QuizSession.where(user: current_user, quiz: @quiz).order(end_time: :desc).first
-    if @quiz_session && @quiz_session.end_time
+    @quiz_session = find_last_completed_session
+    if @quiz_session&.end_time
       @user_answers = @quiz_session.user_answers
     else
       redirect_to quizzes_path, alert: I18n.t('alerts.no_completed_session')
@@ -58,16 +56,15 @@ class QuizzesController < ApplicationController
   end
 
   def start
-    existing_session = QuizSession.find_by(user: current_user, quiz: @quiz, end_time: nil)
-    existing_session.update(end_time: Time.current) if existing_session
+    end_existing_session
     @quiz_session = QuizSession.create(user: current_user, quiz: @quiz, start_time: Time.current)
     redirect_to take_quiz_path(@quiz, question_id: @quiz.questions.first.id, resume_session: "false")
   end
 
   def resume
-    existing_session = QuizSession.find_by(user: current_user, quiz: @quiz, end_time: nil)
-    last_answered_question_id = existing_session.user_answers.last&.question_id
-    next_question = last_answered_question_id ? @quiz.questions.where('questions.id > ?', last_answered_question_id).first : @quiz.questions.first
+    existing_session = find_existing_session
+    last_answered_question_id = existing_session&.user_answers&.last&.question_id
+    next_question = find_next_question(last_answered_question_id)
     redirect_to take_quiz_path(@quiz, question_id: next_question.id, resume_session: "false")
   end
 
@@ -85,5 +82,36 @@ class QuizzesController < ApplicationController
 
   def quiz_params
     params.require(:quiz).permit(:title, :description)
+  end
+
+  def find_existing_session
+    QuizSession.find_by(user: current_user, quiz: @quiz, end_time: nil)
+  end
+
+  def resume_session?
+    @existing_session && params[:resume_session] != "false"
+  end
+
+  def start_new_session
+    @existing_session ||= QuizSession.create(user: current_user, quiz: @quiz, start_time: Time.current)
+    @question = params[:question_id] ? @quiz.questions.find(params[:question_id]) : @quiz.questions.first
+    @user_answer = UserAnswer.new
+  end
+
+  def find_last_completed_session
+    QuizSession.where(user: current_user, quiz: @quiz).order(end_time: :desc).first
+  end
+
+  def end_existing_session
+    existing_session = find_existing_session
+    existing_session&.update(end_time: Time.current)
+  end
+
+  def find_next_question(last_answered_question_id)
+    if last_answered_question_id
+      @quiz.questions.where('questions.id > ?', last_answered_question_id).first
+    else
+      @quiz.questions.first
+    end
   end
 end
