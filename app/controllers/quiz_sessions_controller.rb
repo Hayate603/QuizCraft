17fe71ec2a443c2fa1187c39458586d67fd7a2fc
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-04 11:07:42 +0900

    [update]quiz_sessions_controller の update アクションの翻訳対応
    - update アクションのメッセージを翻訳ファイルに移動
    - クイズセッション終了時の通知メッセージを I18n を使用して表示
    - エラー発生時のメッセージも翻訳対応

--- app/controllers/quiz_sessions_controller.rb
+++ app/controllers/quiz_sessions_controller.rb
@@ -2,21 +2,12 @@ class QuizSessionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_quiz
 
-  def create
-    @quiz_session = @quiz.quiz_sessions.build(user: current_user, start_time: Time.current)
-    if @quiz_session.save
-      redirect_to @quiz_session, notice: "Quiz session started!"
-    else
-      redirect_to @quiz, alert: "Failed to start quiz session."
-    end
-  end
-
   def update
     @quiz_session = QuizSession.find(params[:id])
     if @quiz_session.update(end_time: Time.current)
-      redirect_to quiz_results_path(@quiz_session.quiz), notice: "Quiz session ended!"
+      redirect_to quiz_results_path(@quiz_session.quiz), notice: I18n.t('notices.quiz_session_ended')
     else
-      redirect_to @quiz_session, alert: "Failed to end quiz session."
+      redirect_to @quiz_session, alert: I18n.t('alerts.end_quiz_session_failed')
     end
   end
 
