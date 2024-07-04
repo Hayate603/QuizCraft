9cb7f94fda3eb13592284c7d32bcdb9bcadbf4e1
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-04 11:06:01 +0900

    [update]user_answers_controller の翻訳対応と既存の回答チェックを追加
    - メッセージを翻訳ファイルに移動
    - 既存の回答チェックを追加し、重複回答を防止
    - 回答送信時の通知メッセージを I18n を使用して表示

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -8,12 +8,21 @@ class UserAnswersController < ApplicationController
     @user_answer.user = current_user
     @user_answer.quiz = @quiz
     @user_answer.quiz_session = current_quiz_session
-    @user_answer.correct = @user_answer.answer_text == @question.correct_answer
 
-    if @user_answer.save
-      redirect_to next_question_path, notice: "Answer submitted!"
+    existing_answer = UserAnswer.find_by(user: current_user, question: @question, quiz_session: @user_answer.quiz_session)
+    if existing_answer
+      redirect_to take_quiz_path(@quiz, question_id: @question.id), alert: I18n.t('alerts.already_answered')
+    elsif @user_answer.save
+      if last_question?
+        current_quiz_session.update(end_time: Time.current)
+        redirect_to results_quiz_path(@quiz), notice: I18n.t('notices.quiz_completed')
+      else
+        next_question = @quiz.questions.where('questions.id > ?', @question.id).first
+        redirect_to take_quiz_path(@quiz, question_id: next_question.id, resume_session: "false"), notice: I18n.t('notices.answer_submitted')
+      end
     else
-      render "questions/show"
+      @questions = @quiz.questions
+      render "quizzes/take"
     end
   end
 
@@ -31,11 +40,13 @@ class UserAnswersController < ApplicationController
     params.require(:user_answer).permit(:answer_text)
   end
 
-  def next_question_path
-    # ロジックに応じて次の質問または結果ページへのパスを返す
+  def last_question?
+    @quiz.questions.where('questions.id > ?', @question.id).empty?
   end
 
   def current_quiz_session
-    # 現在のクイズセッションを取得するロジックを追加
+    QuizSession.find_or_create_by(user: current_user, quiz: @quiz, end_time: nil) do |session|
+      session.start_time = Time.current
+    end
   end
 end
