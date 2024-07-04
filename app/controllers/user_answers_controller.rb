547e3f61ebc06dc1937e16639c3c7a2bbb72f8b7
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-04 19:23:33 +0900

    [update]user_answers_controllerのリファクタリング
    - ユーザー回答生成ロジックをbuild_user_answerに分割
    - 既存回答検索ロジックをexisting_answerに分割
    - 既存回答リダイレクトロジックをredirect_to_existing_answerに分割
    - 回答保存処理をhandle_answer_savingに分割
    - 保存失敗処理をhandle_saving_failureに分割
    - 次の質問検索ロジックをfind_next_questionに分割

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -4,25 +4,14 @@ class UserAnswersController < ApplicationController
   before_action :set_question
 
   def create
-    @user_answer = @question.user_answers.build(user_answer_params)
-    @user_answer.user = current_user
-    @user_answer.quiz = @quiz
-    @user_answer.quiz_session = current_quiz_session
+    @user_answer = build_user_answer
 
-    existing_answer = UserAnswer.find_by(user: current_user, question: @question, quiz_session: @user_answer.quiz_session)
     if existing_answer
-      redirect_to take_quiz_path(@quiz, question_id: @question.id), alert: I18n.t('alerts.already_answered')
+      redirect_to_existing_answer
     elsif @user_answer.save
-      if last_question?
-        current_quiz_session.update(end_time: Time.current)
-        redirect_to results_quiz_path(@quiz), notice: I18n.t('notices.quiz_completed')
-      else
-        next_question = @quiz.questions.where('questions.id > ?', @question.id).first
-        redirect_to take_quiz_path(@quiz, question_id: next_question.id, resume_session: "false"), notice: I18n.t('notices.answer_submitted')
-      end
+      handle_answer_saving
     else
-      @questions = @quiz.questions
-      render "quizzes/take"
+      handle_saving_failure
     end
   end
 
@@ -48,5 +37,42 @@ class UserAnswersController < ApplicationController
     QuizSession.find_or_create_by(user: current_user, quiz: @quiz, end_time: nil) do |session|
       session.start_time = Time.current
     end
+  end
+
+  def build_user_answer
+    @question.user_answers.build(user_answer_params).tap do |user_answer|
+      user_answer.user = current_user
+      user_answer.quiz = @quiz
+      user_answer.quiz_session = current_quiz_session
+    end
+  end
+
+  def existing_answer
+    UserAnswer.find_by(user: current_user, question: @question, quiz_session: @user_answer.quiz_session)
+  end
+
+  def redirect_to_existing_answer
+    redirect_to take_quiz_path(@quiz, question_id: @question.id), alert: I18n.t('alerts.already_answered')
+  end
+
+  def handle_answer_saving
+    if last_question?
+      current_quiz_session.update(end_time: Time.current)
+      redirect_to results_quiz_path(@quiz), notice: I18n.t('notices.quiz_completed')
+    else
+      next_question = find_next_question
+      redirect_to take_quiz_path(
+        @quiz, question_id: next_question.id, resume_session: "false"
+      ), notice: I18n.t('notices.answer_submitted')
+    end
+  end
+
+  def handle_saving_failure
+    @questions = @quiz.questions
+    render "quizzes/take"
+  end
+
+  def find_next_question
+    @quiz.questions.where('questions.id > ?', @question.id).first
   end
 end
