aca1770264eb03a7ac9eb4316e180f648d9fc453
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-06 10:23:14 +0900

    [update]お気に入り機能のリダイレクト先を調整
    - クイズ一覧ページからお気に入りを追加/削除する際に、クイズ詳細ページにリダイレクトされる問題を修正
    - `redirect_back` メソッドを使用して、元のページにリダイレクトするように変更

--- app/controllers/favorite_quizzes_controller.rb
+++ app/controllers/favorite_quizzes_controller.rb
@@ -8,12 +8,12 @@ class FavoriteQuizzesController < ApplicationController
   def create
     quiz = Quiz.find(params[:quiz_id])
     current_user.favorite_quizzes.create!(quiz: quiz)
-    redirect_to quiz, notice: 'クイズをお気に入りに追加しました。'
+    redirect_back fallback_location: quiz, notice: 'クイズをお気に入りに追加しました。'
   end
 
   def destroy
     quiz = Quiz.find(params[:quiz_id])
     current_user.favorite_quizzes.find_by(quiz: quiz).destroy
-    redirect_to quiz, notice: 'クイズをお気に入りから削除しました。'
+    redirect_back fallback_location: quiz, notice: 'クイズをお気に入りから削除しました。'
   end
 end
