3870bfb29f2e8881aaaabe7033149c8b4d6dd5a0
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-06 19:53:45 +0900

    [update]Quizモデルのfavorite_quizzesアソシエーションにdependent: :destroyオプションを追加。

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,7 +4,7 @@ class Quiz < ApplicationRecord
   has_many :questions, through: :quiz_questions
   has_many :quiz_sessions, dependent: :destroy
   has_many :user_answers, dependent: :destroy
-  has_many :favorite_quizzes
+  has_many :favorite_quizzes, dependent: :destroy
   has_many :favorited_by, through: :favorite_quizzes, source: :user
 
   validates :title, presence: true, uniqueness: { scope: :user_id }
