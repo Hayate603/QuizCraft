ad948c33c7b680d1b4db42875efd7cb072689fab
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-06 13:18:40 +0900

    [update]FavoriteQuizモデルのバリデーション追加
    - `user`と`quiz`が存在することをバリデーションとして追加

--- app/models/favorite_quiz.rb
+++ app/models/favorite_quiz.rb
@@ -3,4 +3,6 @@ class FavoriteQuiz < ApplicationRecord
   belongs_to :quiz
 
   validates :user_id, uniqueness: { scope: :quiz_id }
+  validates :user, presence: true
+  validates :quiz, presence: true
 end
