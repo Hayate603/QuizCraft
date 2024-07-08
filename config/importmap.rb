bdf4d4eec8fc058f15527aea1eb78de48b47eacd
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-07-08 19:35:35 +0900

    [update] importmap.rb に image_upload.js のパスを追加
    - image_upload.js のパスを追加し、JavaScript を利用可能に

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/ujs", to: "rails-ujs.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "image_upload", to: "image_upload.js", preload: true
