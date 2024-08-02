22cf270461da1ad84e7b8c3c03ba6e5e0f24dc75
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-08-02 22:19:27 +0900

    [update]FactoryBotでのユーザーファクトリにquiz_modeのデフォルト設定を追加
    - ユーザーファクトリでquiz_modeのデフォルト値をUser::QUIZ_MODES[:default]に設定
    - テストデータ作成時のバリデーションエラーを防止

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:email) { |n| "user#{n}@example.com" }
     password { "password" }
     confirmed_at { Time.zone.now }
+    quiz_mode { User::QUIZ_MODES[:default] }
 
     trait :sns_user do
       provider { "google_oauth2" }
