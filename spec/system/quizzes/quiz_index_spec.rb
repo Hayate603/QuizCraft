b0c484c96ae8f2cb5e8832ecc610a0946edb59e4
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-09-05 13:13:54 +0900

    [add]クイズ一覧ページのテストに公開、非公開の表示に関するテストを追加
    - クイズ一覧ページで公開クイズのみが表示され、非公開クイズは表示されないことを確認するテストを追加
    - 公開クイズをお気に入りに追加・削除できる機能のテストを修正

--- spec/system/quizzes/quiz_index_spec.rb
+++ spec/system/quizzes/quiz_index_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Quiz Index', type: :system do
   let(:user) { create(:user) }
-  let!(:quiz) { create(:quiz, user:, title: 'Sample Quiz', description: 'Sample description') }
+  let!(:published_quiz) { create(:quiz, user:, title: '公開クイズ', publish: true) }
+  let!(:unpublished_quiz) { create(:quiz, user:, title: '非公開クイズ', publish: false) }
 
   describe 'クイズ一覧の表示' do
-    it 'クイズの一覧が表示されること' do
+    it '公開クイズが表示され、非公開クイズは表示されないこと' do
       visit quizzes_path
-      expect(page).to have_content 'Sample Quiz'
-      expect(page).to have_link 'Sample Quiz', href: quiz_path(quiz)
+      expect(page).to have_content '公開クイズ'
+      expect(page).not_to have_content '非公開クイズ'
     end
   end
 
@@ -20,16 +21,16 @@ RSpec.describe 'Quiz Index', type: :system do
 
       it 'クイズをお気に入りに追加できること' do
         visit quizzes_path
-        within(".quizzes__item--#{quiz.id}") do
+        within(".quizzes__item--#{published_quiz.id}") do
           click_link 'お気に入りに追加'
         end
         expect(page).to have_content('クイズをお気に入りに追加しました。')
       end
 
       it 'クイズをお気に入りから削除できること' do
-        user.favorite_quizzes.create!(quiz:)
+        user.favorite_quizzes.create!(quiz: published_quiz)
         visit quizzes_path
-        within(".quizzes__item--#{quiz.id}") do
+        within(".quizzes__item--#{published_quiz.id}") do
           click_link 'お気に入りから削除'
         end
         expect(page).to have_content('クイズをお気に入りから削除しました。')
