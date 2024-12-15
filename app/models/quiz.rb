4d1b40bd0842c52975f8e79b50857d219741387c
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-12-15 17:38:12 +0900

    [remove]Quizモデルからquiz_sessionsの関連を削除
    - quiz_sessionsモデルの廃止に伴う関連の削除。

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,7 +2,6 @@ class Quiz < ApplicationRecord
   belongs_to :user
   has_many :quiz_questions, dependent: :destroy
   has_many :questions, through: :quiz_questions
-  has_many :quiz_sessions, dependent: :destroy
   has_many :favorite_quizzes, dependent: :destroy
   has_many :favorited_by, through: :favorite_quizzes, source: :user
 
