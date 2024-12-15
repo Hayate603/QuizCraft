681a4f9b67f40a97f83899c51878d5e8f010bb78
Hayate Fukashiro <hayatefukashiro@MacBook-Air.local>
2024-12-15 17:02:55 +0900

    [add]セッションストアをActiveRecordに変更
    - activerecord-session_store を導入
    - セッションストアをActiveRecordストアに設定
    - セッション用のテーブルを作成するマイグレーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_04_045100) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_15_053752) do
   create_table "favorite_quizzes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "quiz_id", null: false
@@ -57,6 +57,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_04_045100) do
     t.boolean "publish", default: false, null: false
     t.index ["user_id", "title"], name: "index_quizzes_on_user_id_and_title", unique: true
     t.index ["user_id"], name: "index_quizzes_on_user_id"
+  end
+
+  create_table "sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "user_answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
